1ef79660a36fb5d2c39e307c41b310f9f9ca306b
ahmeddrabeii <ahma456ad@gmail.com>
2026-08-03 19:49:09 +0300

    Add project files.

---
+++ C# Basics-Session04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__Basics_Session04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
